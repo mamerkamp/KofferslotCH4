8e766ba5cc7a9e005fa1c6ea73babb0c1e6add55
markamerkamp <markamerkamp@gmail.com>
2020-09-26 20:26:58 +0200

    Uitwerking opdracht 2.2 Kofferslot compleet

--- src/kofferslot/Kofferslot.java
+++ src/kofferslot/Kofferslot.java
@@ -5,4 +5,73 @@ package kofferslot;
  * Veel plezier ermee!
  */
 public class Kofferslot {
+    private char eersteLetter;
+    private char tweedeLetter;
+    private int cijfer;
+
+    public Kofferslot(char eersteLetter, char tweedeLetter, int cijfer) {
+        this.eersteLetter = eersteLetter;
+        this.tweedeLetter = tweedeLetter;
+        this.cijfer = cijfer;
+        setCombinatie(eersteLetter, tweedeLetter, cijfer);
+
+    }
+
+    public Kofferslot() {
+        this('A', 'A', 0);
+    }
+
+    public void setCombinatie(char eersteLetter, char tweedeLetter, int cijfer) {
+        this.eersteLetter = getEersteLetter();
+        this.tweedeLetter = getTweedeLetter();
+        this.cijfer = getCijfer();
+    }
+    public char volgendeLetter(char letter) {
+        if (letter == 'Z'){
+            return 'A';
+        }
+        return ++letter;
+    }
+
+    public int volgendeCijfer(int cijfer) {
+        if (cijfer == 9){
+            return 0;
+        }
+        return ++cijfer;
+    }
+
+    public void volgende() {
+        cijfer = volgendeCijfer(cijfer);
+        if (cijfer == 0) {
+            tweedeLetter = volgendeLetter(tweedeLetter);
+            if (tweedeLetter == 'A') {
+                eersteLetter = volgendeLetter(eersteLetter);
+            }
+        }
+
+    }
+
+    public char getEersteLetter() {
+        return eersteLetter;
+    }
+
+    public void setEersteLetter(char eersteLetter) {
+        this.eersteLetter = eersteLetter;
+    }
+
+    public char getTweedeLetter() {
+        return tweedeLetter;
+    }
+
+    public void setTweedeLetter(char tweedeLetter) {
+        this.tweedeLetter = tweedeLetter;
+    }
+
+    public int getCijfer() {
+        return cijfer;
+    }
+
+    public void setCijfer(int cijfer) {
+        this.cijfer = cijfer;
+    }
 }
--- src/kofferslot/KofferslotLauncher.java
+++ src/kofferslot/KofferslotLauncher.java
@@ -5,4 +5,22 @@ package kofferslot;
  * Veel plezier ermee!
  */
 public class KofferslotLauncher {
+
+    public static void main(String[] args) {
+
+//        Kofferslot kofferslot = new Kofferslot();
+//
+//        System.out.println(kofferslot.getEersteLetter());
+//        System.out.println(kofferslot.getTweedeLetter());
+//        System.out.println(kofferslot.getCijfer());
+
+        Kofferslot kofferslot2 = new Kofferslot('Z', 'Z', 9);
+        kofferslot2.volgende();
+        System.out.println(kofferslot2.getEersteLetter());
+        System.out.println(kofferslot2.getTweedeLetter());
+        System.out.println(kofferslot2.getCijfer());
+
+
+    }
+
 }
